bc282da24db85ed79610cdfbcc8f04216a0c11e6
hoyyChoi <hoyeon8165@gmail.com>
2024-11-10 15:13:11 +0900

    fix : portnumber 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "billeasy",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -p 3001",
       cwd: "./",
       instances: 1,
       autorestart: true,
