daabc4606d458088403d5a0b4eede46e0d26c36a
hoyyChoi <hoyeon8165@gmail.com>
2024-11-10 15:14:02 +0900

    fix : deploy.sh pm2 명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ cd $REPOSITORY
 
 sudo npm install
 
-sudo pm2 start ecosystem.config.js --env production || sudo pm2 reload ecosystem.config.js --env production
+sudo pm2 start ecosystem.config.js || sudo pm2 reload ecosystem.config.js
