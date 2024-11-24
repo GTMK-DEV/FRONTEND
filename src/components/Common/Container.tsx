7ebe84ae46266812645226f0fda6b2395f6924ef
hoyyChoi <hoyeon8165@gmail.com>
2024-11-24 13:07:37 +0900

    style : Container style 초기화

--- src/components/Common/Container.tsx
+++ src/components/Common/Container.tsx
@@ -2,12 +2,7 @@ import { pretendard } from '@/styles/font';
 import React from 'react';
 
 const Container = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main
-      className={`${pretendard.className} bg bg-background-main mx-auto h-screen max-w-[430px]`}>
-      {children}
-    </main>
-  );
+  return <main className={`${pretendard.className} `}>{children}</main>;
 };
 
 export default Container;
