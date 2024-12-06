2f59e21afa21d57b1d0b9a1f39cfaaf89f5080ad
hoyyChoi <hoyeon8165@gmail.com>
2024-12-06 21:09:40 +0900

    feat : Logo 컴포넌트 추출

---
+++ src/components/Common/Logo.tsx
@@ -0,0 +1,10 @@
+import BilleasyLogo from '../../../public/images/logos/billeasy_logo.svg';
+import BilleasyTextLogo from '../../../public/images/logos/billeasy_text_logo.svg';
+import RegisterLogo from '../../../public/images/temp/register_icon.svg';
+const Logo = {
+  BilleasyLogo,
+  BilleasyTextLogo,
+  RegisterLogo
+};
+
+export default Logo;
